52ff6917e087b069d4290bb0d1ea23c0153ebf86
“Vilde <vilde@vevatne.no>
2018-06-17 11:54:18 -0700

    Use each char instead

--- algos.rb
+++ algos.rb
@@ -3,14 +3,14 @@
 def sort_by_string(s, t)
 	s_chars = {}
 	new_str = ''
-	s.split('').each do |char|
+	s.each_char do |char|
 		if s_chars[char]
 			s_chars[char] += 1
 		else 
 			s_chars[char] = 1
 		end 
 	end
-	t.split('').each do |char|
+	t.each_char do |char|
 		if s_chars[char]
 			new_str.concat(char * s_chars[char])
 		end 
