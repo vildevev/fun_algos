2ae04b2a8147bf5541a6c1025a225043a3a243c0
“Vilde <vilde@vevatne.no>
2018-06-17 12:25:16 -0700

    Account for edge cases in sort str

--- algos.rb
+++ algos.rb
@@ -1,7 +1,10 @@
-# Time complexity = O(mn)
+# Time complexity = O(m + n)
 # Space complexity = O(n)
 def sort_by_string(s, t)
+  # return the first string if the second is empty or not a string
+  return s if !t.is_a? String or t == ''
 	s_chars = {}
+  additional_chars = []
 	new_str = ''
 	s.each_char do |char|
 		if s_chars[char]
@@ -13,8 +16,17 @@ def sort_by_string(s, t)
 	t.each_char do |char|
 		if s_chars[char]
 			new_str.concat(char * s_chars[char])
+      s_chars.delete(char)
 		end 
-	end 
+	end
+  if !s_chars.empty?
+    # If the first string contains characters that are not present in the second string, add them in alphabetical order at the end
+    s_chars.map do |k,v| 
+      additional_chars.push(k * v)
+    end
+    additional_chars.sort!
+    additional_chars.each { |char| new_str.concat(char) }
+  end  
 	new_str
 end
 
@@ -51,7 +63,9 @@ def change_possibilities(target, nums)
 			possible += 1 
 		elsif num + nums[i+1] == target 
 			possible += 1 
-		else 
+		else
+		end 
+	end 
 end 
 
 change_possibilities(4, [1,2,3])
